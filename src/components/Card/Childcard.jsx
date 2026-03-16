ae931849efa055717bd06502ba6c058e3d9c39d2
unknown <sajinalama499@gmail.com>
2026-03-16 23:34:05 +0545

    ui responsiveness fix

--- src/components/Card/Childcard.jsx
+++ src/components/Card/Childcard.jsx
@@ -33,9 +33,9 @@ const Childcard = ({title,bgcolor,description,orientation,num,textcolor,images,}
           </div>
         </div>
       ) : (
-       <div className={`w-full max-w-xl lg:h-124  rounded-3xl p-10 ${bgcolor} ${textcolor}  `}>
-        <div className="flex flex-col relative -rotate-90 top-9-left-20 ">
-          <div className="text-4xl pt-3 font-bold">
+       <div className={`w-full max-w-xl lg:h-124 h-130 rounded-3xl p-10 ${bgcolor} ${textcolor}  `}>
+        <div className="flex flex-col relative -rotate-90 lg:top-9 top-22 lg:-left-30 -left-20 ">
+          <div className="lg:text-4xl text-2xl pt-3 font-bold text-wrap">
             {title}
           </div>
           <div className="text-[18px] mt-4 font-semibold ">
@@ -43,8 +43,8 @@ const Childcard = ({title,bgcolor,description,orientation,num,textcolor,images,}
           </div>
           
         </div>
-        <div className="relative mt-19">
-          <span className="lg:text-[10rem] text-[8rem] font-bold leading-none"> {num}</span>
+        <div className="relative lg:mt-19 mt-55 ">
+          <span className="lg:text-[10rem] text-[7rem] font-bold leading-none"> {num}</span>
               <span className="absolute -top-2 lg:-right-4  text-6xl font-bold">
                 +
               </span>
